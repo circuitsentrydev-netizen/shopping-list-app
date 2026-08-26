3c0548e1353b4bda55a3e5f2f7f6945447b4f4b2
circuitsentrydev-netizen <circuitsentrydev@gmail.com>
2026-08-26 15:48:20 +0200

    store ts

--- src/features/store/hook.ts
+++ src/features/store/hook.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 // 1. Corrected the path to use a relative dot-slash notation
-import type { AppDispatch, RootState } from './store'
+import type { AppDispatch, RootState } from './store'   
 
 // 2. Added parentheses () to invoke the factory method correctly
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
