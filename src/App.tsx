0cc10a0b1f63e6e94a9f7cb02b0bf7ca63a8c533
circuitsentrydev-netizen <circuitsentrydev@gmail.com>
2026-08-26 15:40:30 +0200

    fixed file path store

--- src/App.tsx
+++ src/App.tsx
@@ -11,58 +11,50 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Settings from "./pages/Settings";
+import ListDetails from "./pages/listDetails";
 
 function ProtectedRoute({ children }: { children: ReactNode }) {
-  const authenticated = useAppSelector(
-    (state: RootState) => state.auth.isAuthenticated
-  );
-
+  const authenticated = useAppSelector((state: RootState) => state.auth.isAuthenticated);
   return authenticated ? <>{children}</> : <Navigate to="/login" replace />;
 }
 
 function PublicRoute({ children }: { children: ReactNode }) {
-  const authenticated = useAppSelector(
-    (state: RootState) => state.auth.isAuthenticated
-  );
-
+  const authenticated = useAppSelector((state: RootState) => state.auth.isAuthenticated);
   return authenticated ? <Navigate to="/" replace /> : <>{children}</>;
 }
 
 export default function App() {
-  const authenticated = useAppSelector(
-    (state: RootState) => state.auth.isAuthenticated
-  );
+  const authenticated = useAppSelector((state: RootState) => state.auth.isAuthenticated);
 
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        <Route path="/categories" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
-        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+        <Route path="/" element={<ProtectedRoute>
+          <Home />
+
+        </ProtectedRoute>} />
+        <Route path="/login" element={<PublicRoute><Login />
+        </PublicRoute>} />
+
+        <Route path="/register" element={<PublicRoute>
+          <Register />
+          </PublicRoute>} />
+
+        <Route path="/profile" element={<ProtectedRoute>
+          <Profile />
+          </ProtectedRoute>} />
+
+        <Route path="/categories" element={<ProtectedRoute>
+          <Categories /></ProtectedRoute>} />
+
+        <Route path="/settings" element={<ProtectedRoute>
+          <Settings />
+          </ProtectedRoute>} />
+
+        <Route path="/list-details" element={<ProtectedRoute>
+          <ListDetails />
+          </ProtectedRoute>} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
