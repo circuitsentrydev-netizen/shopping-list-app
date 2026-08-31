b09d210a8fe1317d02fdddc8062b66118708a982
circuitsentrydev-netizen <circuitsentrydev@gmail.com>
2026-08-31 09:26:50 +0200

    renamed shoppinglistslice

--- src/features/store/store.ts
+++ src/features/store/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../authSlice'
-import shoppingListReducer from '../shoppingListSlice';
+import authReducer from './authSlice';
+import userReducer from './userSlice'; 
+import shoppingItemsReducer from '../shoppingListSlice'; //  Clearer naming matching list items
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    shoppingList: shoppingListReducer,
+    user: userReducer, 
+    shoppingItems: shoppingItemsReducer, //  Exchanged 'shoppingList' for 'shoppingItems'
   },
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
